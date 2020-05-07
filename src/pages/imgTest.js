8be11f2d2ce260c45712b0bfb181432ae1a23db0
James Brittan <jamesbrittan@gmail.com>
2020-05-07 14:14:21 +0100

    added basic func

--- src/pages/imgTest.js
+++ src/pages/imgTest.js
@@ -1,8 +1,42 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-const imgTest = () => {
+
+const ImgTest = () => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [err, setErr] = useState("")
+
+  useEffect(() => {
+    setLoading(true)
+    fetch("/.netlify/functions/generate-image", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setLoading(false)
+        setData(json)
+      })
+      .catch(err => {
+        if (window.location.origin === "http://localhost:8000")
+          setErr(
+            'your origin is "http://localhost:8000". You are likely not using Netlify Dev so the functions server isnt running. Please read the docs, use Netlify Dev, and go to http://localhost:8888'
+          )
+        else setErr(err)
+        throw err
+      })
+    return () => {
+      console.log(data);
+    }
+  }, [])
+
+
+
   return (
     <Layout>
       <SEO title="Local image stats test" />
@@ -11,4 +45,4 @@ const imgTest = () => {
   )
 }
 
-export default imgTest
+export default ImgTest
