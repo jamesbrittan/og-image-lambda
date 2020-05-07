4e08d3425ab40788a76242e5bb196a67eeea8b8c
James Brittan <jamesbrittan@gmail.com>
2020-05-07 13:18:48 +0100

    added basic imgTest page comp

---
+++ src/pages/imgTest.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+const imgTest = () => {
+  return (
+    <Layout>
+      <SEO title="Local image stats test" />
+      <div>test</div>
+    </Layout>
+  )
+}
+
+export default imgTest
